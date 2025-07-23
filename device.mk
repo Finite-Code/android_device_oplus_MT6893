c2e37699255d729aea7abfbba1bb7112f92cc8bb
liwhy1 <levi.toth2005@gmail.com>
2025-07-23 22:52:31 +0200

    MT6893: Build mtk vibrator hal

--- device.mk
+++ device.mk
@@ -494,7 +494,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    android.hardware.vibrator-service.mt6893
 
 # VNDK
 PRODUCT_PACKAGES += \
