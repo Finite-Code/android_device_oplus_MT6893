c7c0e155f41f92ef0540e49cd1e096851aa1b220
liwhy1 <levi.toth2005@gmail.com>
2025-08-21 14:09:02 +0200

    MT6893: Merge in upstream

--- device.mk
+++ device.mk
@@ -367,7 +367,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/publiclibraries/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Power
-$(call inherit-product, $(COMMON_PATH)/power-libperfmgr/power-libperfmgr.mk)
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.pixel-libperfmgr
 
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub
@@ -461,9 +462,11 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH) \
-    $(COMMON_PATH) \
-    hardware/mediatek
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
+    $(DEVICE_PATH)
 
 # IMS
 PRODUCT_BOOT_JARS += \
--- init/init_MT6893.cpp
+++ init/init_MT6893.cpp
@@ -107,11 +107,8 @@ void vendor_load_properties() {
     // Optional: global ro.device override
     property_override("ro.vendor.device", device.c_str());
 	
-    // Set gamma conversion prop
+    // Set gamma conversion prop for denniz
     if (device == "denniz") {
         property_override("sys.brightness.disable_gamma_conversion", "0");
-    }
-    else {
-        property_override("sys.brightness.disable_gamma_conversion", "1");
     }
 }
