0f5b1774a2c2e8396491636fc94482c25a0bb077
liwhy1 <levi.toth2005@gmail.com>
2025-12-22 23:48:24 +0100

    MT6893: sensors: Drop AlsCorrection
    * seems unused, service isn't able to start in its current state

--- device.mk
+++ device.mk
@@ -444,7 +444,6 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service-multihal.MT6893 \
-    als_correction_service.MT6893 \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
