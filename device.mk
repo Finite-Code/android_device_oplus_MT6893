12b87cc1284403293dd7aba4135807255d1f7712
liwhy1 <levi.toth2005@gmail.com>
2025-12-27 16:49:35 +0100

    MT6893: health: Swap charging state value

--- device.mk
+++ device.mk
@@ -257,8 +257,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
