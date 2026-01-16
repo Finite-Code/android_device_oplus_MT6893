c74bb23e012668d30ae073acd7e3d12af7db964e
liwhy1 <levi.toth2005@gmail.com>
2026-01-16 22:07:28 +0100

    MT6893: extract-files: Patch vendor.mediatek.hardware.pq@2.15-impl.so to load libutils_v32.so

--- extract-files.py
+++ extract-files.py
@@ -25,6 +25,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service': blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so')
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
     'vendor/lib*/hw/audio.primary.mt6893.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v31.so')
         .replace_needed('libtinyalsa.so', 'libtinyalsa-v32.so'),
