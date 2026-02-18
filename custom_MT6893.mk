96d8ff7dad2fa41164f755b71ddb9369febaeba2
Atharva Pandey <93546392+Finite-Code@users.noreply.github.com>
2026-02-18 11:48:19 +0530

    Comment out common_full_phone.mk inclusion
    Comment out the inclusion of common_full_phone.mk.

--- custom_MT6893.mk
+++ custom_MT6893.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oplus/MT6893/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+# $(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := custom_MT6893
